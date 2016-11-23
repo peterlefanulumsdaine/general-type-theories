bb57dccfb798b30eac739349ba5f943fe05efd4d
Théo Winterhalter <isheeft@gmail.com>
2016-11-23 09:25:24 +0100

    Minor editing

--- RawSyntax.v
+++ RawSyntax.v
@@ -1,5 +1,6 @@
 
 Require Import Auxiliary.
+Require Import Vectors.Fin.
 
 (* Background: abstracting proto-contexts *)
 
@@ -11,10 +12,10 @@ Section ProtoCxts.
 Record ProtoCxtSystem :=
   { ProtoCxt :> Type
   ; vars : ProtoCxt -> Type (* should be to sets?? *)
-  ; protocxt_coprod : ProtoCxt -> ProtoCxt -> ProtoCxt 
+  ; protocxt_coprod : ProtoCxt -> ProtoCxt -> ProtoCxt
   ; _ : forall γ δ : ProtoCxt, is_coprod (vars (protocxt_coprod γ δ)) (vars γ) (vars δ)
   ; protocxt_extend : ProtoCxt -> ProtoCxt
-  ; _ : forall γ δ : ProtoCxt, is_extend_by_1 (vars (protocxt_extend γ)) (vars γ)
+  ; _ : forall γ : ProtoCxt, is_extend_by_1 (vars (protocxt_extend γ)) (vars γ)
   }.
 
 Coercion vars : ProtoCxt >-> Sortclass.
@@ -23,7 +24,7 @@ Definition deBruijn : ProtoCxtSystem.
 Proof.
   simple refine (Build_ProtoCxtSystem _ _ _ _ _ _).
   - exact nat.
-  - admit. (* should be Fin *)
+  - exact Fin.t. (* should be fin *)
   - admit. (* should be + *)
   - admit.
   - admit. (* should be +1 *)
@@ -55,8 +56,8 @@ Section Signatures.
     := Family (Syn_Class * PCxt).
 
   (* Entries in the family represent arguments of a constructor; the [ProtoCxt] component represents the variables bound in each argument.
- 
-  For instance, the [Π-intro] rule (i.e. fully annotated λ-abstraction) would have arity [ Family_from_List [(Ty,0), (Ty,1), (Tm,1)] ]; application would have arity [ Family_from_List [(Ty,0), (Ty,1), (Tm,0), (Tm,0)]]. 
+
+  For instance, the [Π-intro] rule (i.e. fully annotated λ-abstraction) would have arity [ Family_from_List [(Ty,0), (Ty,1), (Tm,1)] ]; application would have arity [ Family_from_List [(Ty,0), (Ty,1), (Tm,0), (Tm,0)]].
 
   The use of [Family] instead of just [list] serves two purposes.  Firstly, it abstracts the aspects of the [list] version that we really need, and so makes the code significantly cleaner/more robust.  Secondly, it allows this definition to be re-used for non-finitary syntax, although we do not intend to explore that for now. *)
 
@@ -68,7 +69,7 @@ Section Signatures.
   (* Alternatives:
     := { Symbols : Syn_Class -> Arity -> Type }.
 
-  or 
+  or
     := { Symbols : Syn_Class -> Type
        ; arity : forall cl, Symbols cl -> Arity }.
 
@@ -81,18 +82,22 @@ Section Signatures.
 End Signatures.
 
 Section Raw_Syntax.
-  
+
+  (* If I put these in their respective section, it doesn't apply in this
+   * one...  *)
+  Arguments class {_} _.
+  Arguments arity {_} _.
+  Arguments protocxt_coprod {_} _ _.
+
   Inductive Raw_Syntax (Σ : Signature)
   : Syn_Class -> PCxt -> Type
     :=
-    | var_raw (γ : PCxt) (i : γ) 
+    | var_raw (γ : PCxt) (i : γ)
         : Raw_Syntax Σ Tm γ
     | symb_raw (γ : PCxt) (S : Symbol Σ)
-               (args : forall (i : arity _ S),
-                   Raw_Syntax Σ (fst (arity _ S i))
-                                (protocxt_coprod _ γ (snd (arity _ S i))))
-      : Raw_Syntax Σ (class _ S) γ.
-(* TODO : make arguments to [arity], [protocxt_coprod], and [class] implicit. *)
+               (args : forall (i : arity S),
+                   Raw_Syntax Σ (fst (arity S i))
+                                (protocxt_coprod γ (snd (arity S i))))
+      : Raw_Syntax Σ (class S) γ.
 
 End Raw_Syntax.
-
