dad87578213bb731a1ada220abdefb2b72cff839
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2016-11-24 15:54:51 +0100

    Renamed ProtoCxtSystems

--- RawSyntax.v
+++ RawSyntax.v
@@ -3,32 +3,33 @@ Require Import Auxiliary.
 
 (* Background: abstracting proto-contexts *)
 
-Section ProtoCxts.
+Section Shape_Systems.
 
-Record ProtoCxtSystem :=
-  { ProtoCxt :> Type
-  ; vars : ProtoCxt -> Type (* maybe should be to sets?? *)
-  ; protocxt_empty : ProtoCxt
-  ; protocxt_is_empty : is_empty (vars protocxt_empty)
-  ; protocxt_coprod : ProtoCxt -> ProtoCxt -> ProtoCxt
-  ; protocxt_is_coprod
-     : forall γ δ : ProtoCxt,
-       is_coprod (vars (protocxt_coprod γ δ)) (vars γ) (vars δ)
-  ; protocxt_extend : ProtoCxt -> ProtoCxt
-  ; protocxt_is_plusone         (* TODO: change to is_extend (Andrej?) *)
-     : forall γ : ProtoCxt,
-       is_plusone (vars (protocxt_extend γ)) (vars γ)
+(* TODO: possibly rename as “context shapes”, “shape systems”, …?  *)
+Record Shape_System :=
+  { Shape :> Type
+  ; positions : Shape -> Type (* maybe should be to sets?? *)
+  ; shape_empty : Shape
+  ; shape_is_empty : is_empty (positions shape_empty)
+  ; shape_coprod : Shape -> Shape -> Shape
+  ; shape_is_coprod
+     : forall γ δ : Shape,
+       is_coprod (positions (shape_coprod γ δ)) (positions γ) (positions δ)
+  ; shape_extend : Shape -> Shape
+  ; shape_is_plusone         (* TODO: change to is_extend (Andrej?) *)
+     : forall γ : Shape,
+       is_plusone (positions (shape_extend γ)) (positions γ)
   }.
 
-Global Arguments protocxt_coprod {_} _ _.
-Global Arguments protocxt_is_coprod {_} [_ _].
-Global Arguments protocxt_is_empty {_}.
+Global Arguments shape_coprod {_} _ _.
+Global Arguments shape_is_coprod {_} [_ _].
+Global Arguments shape_is_empty {_}.
 
-Coercion vars : ProtoCxt >-> Sortclass.
+Coercion positions : Shape >-> Sortclass.
 
-End ProtoCxts.
+End Shape_Systems.
 
-Parameter PCxt : ProtoCxtSystem.
+Parameter Proto_Cxt : Shape_System.
 (* TODO: improve naming *)
 
 Section Signatures.
@@ -36,9 +37,9 @@ Section Signatures.
   Inductive Syn_Class : Type := Ty | Tm.
 
   Definition Arity : Type
-    := Family (Syn_Class * PCxt).
+    := Family (Syn_Class * Proto_Cxt).
 
-  (* Entries in the family represent arguments of a constructor; the [ProtoCxt] component represents the variables bound in each argument.
+  (* Entries in the family represent arguments of a constructor; the [Proto_Cxt] component represents the variables bound in each argument.
 
   For instance, the [Π-intro] rule (i.e. fully annotated λ-abstraction) would have arity [ Family_from_List [(Ty,0), (Ty,1), (Tm,1)] ]; application would have arity [ Family_from_List [(Ty,0), (Ty,1), (Tm,0), (Tm,0)]].
 
@@ -46,7 +47,7 @@ Section Signatures.
 
   (* Access functions for arity *)
   Definition arg_class {a : Arity} (i : a) : Syn_Class := fst (a i).
-  Definition arg_pcxt {a : Arity} (i : a) : PCxt := snd (a i).
+  Definition arg_pcxt {a : Arity} (i : a) : Proto_Cxt := snd (a i).
 
   Definition Signature : Type
     := Family (Syn_Class * Arity).
@@ -57,19 +58,6 @@ Section Signatures.
   Definition arity {Σ : Signature} (S : Σ) : Arity
   := snd (Σ S).
 
-  (* Alternatives:
-    := { Symbols : Syn_Class -> Arity -> Type }.
-
-  or
-    := { Symbols : Syn_Class -> Type
-       ; arity : forall cl, Symbols cl -> Arity }.
-
-  or
-    := { Symbol : Type ;
-         class : Symbol -> Syn_Class ;
-         arity : Symbol -> Arity }.
-  *)
-
 End Signatures.
 
 Section Raw_Syntax.
@@ -78,18 +66,18 @@ Section Raw_Syntax.
 
   (* A raw syntactic expression of a syntactic class, relative to a context *)
   Inductive Raw_Syntax
-    : Syn_Class -> PCxt -> Type
+    : Syn_Class -> Proto_Cxt -> Type
   :=
   (* a variable in a context is a term in that context *)
-    | var_raw (γ : PCxt) (i : γ)
+    | var_raw (γ : Proto_Cxt) (i : γ)
         : Raw_Syntax Tm γ
     (* relative to a context [γ], given a symbol [S], if for each of its
        arguments we have a raw syntactic expression relative to [γ] extended by
        the argument's arity, [S args] is a raw syntactic expression over [γ] *)
-    | symb_raw (γ : PCxt) (S : Σ)
+    | symb_raw (γ : Proto_Cxt) (S : Σ)
                (args : forall (i : arity S),
                    Raw_Syntax (arg_class i)
-                              (protocxt_coprod γ (arg_pcxt i)))
+                              (shape_coprod γ (arg_pcxt i)))
       : Raw_Syntax (class S) γ.
 
   Global Arguments var_raw [_] _.
@@ -98,13 +86,13 @@ Section Raw_Syntax.
   (* A raw context is a proto-ctx ("collection of identifiers") and a raw syntactic type expression 
      for each identifier in the proto-ctx. *)
   Record Raw_Context
-  := { PCxt_of_Raw_Context :> PCxt
+  := { Proto_Cxt_of_Raw_Context :> Proto_Cxt
      ; var_type_of_Raw_Context
-         :> forall i : PCxt_of_Raw_Context,
-            Raw_Syntax Ty PCxt_of_Raw_Context
+         :> forall i : Proto_Cxt_of_Raw_Context,
+            Raw_Syntax Ty Proto_Cxt_of_Raw_Context
      }.
 
-  Definition Raw_Context_Map (γ δ : PCxt)
+  Definition Raw_Context_Map (γ δ : Proto_Cxt)
     := δ -> Raw_Syntax Tm γ.
 
 End Raw_Syntax.
@@ -121,7 +109,7 @@ Section Raw_Subst.
 
   (* Actually easier to define not just weakening, but “weakening + contraction
      + exchange”, i.e. substitution of variables for variables. *)
-  Fixpoint Raw_Weaken {γ γ' : PCxt} (f : γ -> γ')
+  Fixpoint Raw_Weaken {γ γ' : Proto_Cxt} (f : γ -> γ')
       {cl : Syn_Class} (e : Raw_Syntax Σ cl γ)
     : Raw_Syntax Σ cl γ'.
   Proof.
@@ -129,25 +117,25 @@ Section Raw_Subst.
   - exact (var_raw (f i)).
   - refine (symb_raw S _). intros i.
     refine (Raw_Weaken _ _ _ _ (args i)).
-    simple refine (coprod_rect (protocxt_is_coprod) _ _ _); cbn.
-    + intros x. apply (coprod_inj1 (protocxt_is_coprod)). exact (f x).
-    + intros x. apply (coprod_inj2 (protocxt_is_coprod)). exact x.
+    simple refine (coprod_rect (shape_is_coprod) _ _ _); cbn.
+    + intros x. apply (coprod_inj1 (shape_is_coprod)). exact (f x).
+    + intros x. apply (coprod_inj2 (shape_is_coprod)). exact x.
   Defined.
 
-  Definition Raw_Context_Map_Extending (γ γ' δ : PCxt)
+  Definition Raw_Context_Map_Extending (γ γ' δ : Proto_Cxt)
     : Raw_Context_Map Σ γ' γ
-   -> Raw_Context_Map Σ (protocxt_coprod γ' δ) (protocxt_coprod γ δ).
+   -> Raw_Context_Map Σ (shape_coprod γ' δ) (shape_coprod γ δ).
   Proof.
     intros f.
-    simple refine (coprod_rect (protocxt_is_coprod) _ _ _); cbn.
+    simple refine (coprod_rect (shape_is_coprod) _ _ _); cbn.
     - intros i. refine (Raw_Weaken _ (f i)).
-      apply (coprod_inj1 (protocxt_is_coprod)).
+      apply (coprod_inj1 (shape_is_coprod)).
     - intros i. apply var_raw.
-      apply (coprod_inj2 (protocxt_is_coprod)), i.
+      apply (coprod_inj2 (shape_is_coprod)), i.
   Defined.
 
   Fixpoint Raw_Subst
-      {γ γ' : PCxt} (f : Raw_Context_Map Σ γ' γ)
+      {γ γ' : Proto_Cxt} (f : Raw_Context_Map Σ γ' γ)
       {cl : Syn_Class} (e : Raw_Syntax Σ cl γ)
     : Raw_Syntax Σ cl γ'.
   Proof.
@@ -180,37 +168,37 @@ Section Algebraic_Extensions.
     refine (Fmap_Family _ a).
     intros cl_γ. refine (fst cl_γ,_).
     refine {| Inds := snd (cl_γ) ; val := _ |}.
-    intros i. exact (Tm, protocxt_empty _).
+    intros i. exact (Tm, shape_empty _).
   Defined.
 
-  Definition Instantiation (a : Arity) (Σ : Signature) (γ : PCxt)
+  Definition Instantiation (a : Arity) (Σ : Signature) (γ : Proto_Cxt)
     : Type
   := forall i : a,
-       Raw_Syntax Σ (arg_class i) (protocxt_coprod γ (arg_pcxt i)).
+       Raw_Syntax Σ (arg_class i) (shape_coprod γ (arg_pcxt i)).
 
   Definition instantiate
-      {a : Arity} {Σ : Signature} {γ : PCxt}
+      {a : Arity} {Σ : Signature} {γ : Proto_Cxt}
       (I : Instantiation a Σ γ)
       {cl} {δ} (e : Raw_Syntax (Metavariable_Extension Σ a) cl δ)
-    : Raw_Syntax Σ cl (protocxt_coprod γ δ).
+    : Raw_Syntax Σ cl (shape_coprod γ δ).
   Proof.
     induction e as [ δ i | δ [S | M] args Inst_arg ].
   - refine (var_raw _).
-    exact (coprod_inj2 (protocxt_is_coprod) i).
+    exact (coprod_inj2 (shape_is_coprod) i).
   - refine (symb_raw S _). intros i.
     refine (Raw_Weaken _ (Inst_arg i)).
     apply (coprod_assoc
-             protocxt_is_coprod protocxt_is_coprod
-             protocxt_is_coprod protocxt_is_coprod).
+             shape_is_coprod shape_is_coprod
+             shape_is_coprod shape_is_coprod).
   - simpl in M. (* Substitute [args] into the expression [I M]. *)
     refine (Raw_Subst _ (I M)).
-    refine (coprod_rect protocxt_is_coprod _ _ _).
-    + intros i. apply var_raw, (coprod_inj1 protocxt_is_coprod), i.
+    refine (coprod_rect shape_is_coprod _ _ _).
+    + intros i. apply var_raw, (coprod_inj1 shape_is_coprod), i.
     + intros i. 
       refine (Raw_Weaken _ (Inst_arg i)). cbn.
-      refine (fmap_coprod protocxt_is_coprod protocxt_is_coprod _ _).
+      refine (fmap_coprod shape_is_coprod shape_is_coprod _ _).
       exact (fun j => j).
-      exact (coprod_empty_r protocxt_is_coprod protocxt_is_empty).
+      exact (coprod_empty_r shape_is_coprod shape_is_empty).
   Defined.
 
   Global Arguments instantiate {_ _ _} _ [_ _] _.
@@ -226,7 +214,7 @@ Section Judgements.
   Inductive Judgt_Form
     := Cxt_JF | JF (hjf : Hyp_Judgt_Form).
   
-  Definition Hyp_Judgt_Bdry_Instance Σ (hjf : Hyp_Judgt_Form) (γ : PCxt) : Type
+  Definition Hyp_Judgt_Bdry_Instance Σ (hjf : Hyp_Judgt_Form) (γ : Proto_Cxt) : Type
     := match hjf with
          | obj_HJF Ty => unit
          | eq_HJF Ty => (Raw_Syntax Σ Ty γ) * (Raw_Syntax Σ Ty γ)
@@ -234,7 +222,7 @@ Section Judgements.
          | eq_HJF Tm => (Raw_Syntax Σ Ty γ) * (Raw_Syntax Σ Tm γ) * (Raw_Syntax Σ Tm γ) 
        end.
 
-  Definition Hyp_Judgt_Head_Instance Σ (hjf : Hyp_Judgt_Form) (γ : PCxt) : Type
+  Definition Hyp_Judgt_Head_Instance Σ (hjf : Hyp_Judgt_Form) (γ : Proto_Cxt) : Type
     := match hjf with
          | obj_HJF cl => Raw_Syntax Σ cl γ
          | eq_HJF cl => unit
@@ -256,7 +244,7 @@ Section Judgements.
   One option: “slots”.  Other options: ???
 
   If “slots”, then need TODO: infrastructure of families from lists. *)
-  Definition Fmap_Judgt_Instance {Σ Σ'} (f : PCxt -> PCxt)
+  Definition Fmap_Judgt_Instance {Σ Σ'} (f : Proto_Cxt -> Proto_Cxt)
     (g : forall cl γ, Raw_Syntax Σ cl γ -> Raw_Syntax Σ' cl (f γ))
     (h : Raw_Context Σ -> Raw_Context Σ')
   : Judgt_Instance Σ -> Judgt_Instance Σ'.
@@ -309,22 +297,22 @@ Section Raw_Rules.
       refine (Fmap_Judgt_Instance _ _ _).
       + exact (instantiate I).
       + intros Δ.
-        exists (protocxt_coprod Γ Δ).
-        apply (coprod_rect protocxt_is_coprod).
+        exists (shape_coprod Γ Δ).
+        apply (coprod_rect shape_is_coprod).
         * intros i.
           refine (Raw_Weaken _ (Γ i)).
-          exact (coprod_inj1 protocxt_is_coprod).
+          exact (coprod_inj1 shape_is_coprod).
         * intros i.
           exact (instantiate I (Δ i)).
     - (* conclusion *)
       refine (Fmap_Judgt_Instance _ _ _ (RR_concln R)).
       + exact (instantiate I).
       + intros Δ.
-        exists (protocxt_coprod Γ Δ).
-        apply (coprod_rect protocxt_is_coprod).
+        exists (shape_coprod Γ Δ).
+        apply (coprod_rect shape_is_coprod).
         * intros i.
           refine (Raw_Weaken _ (Γ i)).
-          exact (coprod_inj1 protocxt_is_coprod).
+          exact (coprod_inj1 shape_is_coprod).
         * intros i.
           exact (instantiate I (Δ i)).
   Defined.
@@ -332,5 +320,4 @@ Section Raw_Rules.
 End Raw_Rules.
 
 Global Arguments Raw_Rule _ : clear implicits.
-
 
