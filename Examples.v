bee5a1244fe7e9a27aac04889a85bf8b90c29dc3
Andrej Bauer <Andrej.Bauer@andrej.com>
2016-11-25 12:18:00 +0100

    Working on examples.

--- Examples.v
+++ Examples.v
@@ -2,15 +2,18 @@ Require Import Auxiliary.
 Require Import RawSyntax.
 Require Import Arith.
 
-Section TypesAsContexts.
-  Definition TypesProtoCtx : ProtoCxtSystem.
+Section Types_as_shapes.
+  Definition Type_Shape : Shape_System.
   Proof.
     refine {|
-        ProtoCxt := Type ;
-        vars := (fun A => A) ;
-        protocxt_coprod := sum ;
-        protocxt_extend := option
+        Shape := Type ;
+        positions := (fun A => A) ;
+        shape_empty := Empty_set ;
+        shape_coprod := sum ;
+        shape_extend := option
       |}.
+    - constructor.
+      intros P x; elim x.
     - intros A B.
       simple refine {|
           coprod_inj1 := @inl A B ;
@@ -33,102 +36,111 @@ Section TypesAsContexts.
       * reflexivity.
       * reflexivity.
   Defined.
-End TypesAsContexts.
 
+End Types_as_shapes.
 
-Section FreeProtoCxt.
+Section Free_shapes.
 
   Inductive f_cxt : Type :=
-    | f_empty : f_cxt
-    | f_coprod : f_cxt -> f_cxt -> f_cxt
-    | f_extend : f_cxt -> f_cxt.
+  | f_empty : f_cxt
+  | f_coprod : f_cxt -> f_cxt -> f_cxt
+  | f_extend : f_cxt -> f_cxt.
+  
+  Fixpoint f_positions (c : f_cxt) : Type :=
+    match c with
+    | f_empty => Empty_set
+    | f_coprod c d => sum (f_positions c) (f_positions d)
+    | f_extend c => option (f_positions c)
+    end.
 
-  Inductive f_vars : f_cxt -> Type :=
-    | f_inl (c d : f_cxt) : f_vars c -> f_vars (f_coprod c d)
-    | f_inr (c d : f_cxt) : f_vars d -> f_vars (f_coprod c d)
-    | f_top (c : f_cxt) : f_vars (f_extend c)
-    | f_next (c : f_cxt) : f_vars c -> f_vars (f_extend c).
-
-  Definition extend {c d : f_cxt} (P : f_vars (f_coprod c d) -> Type) :
-    forall e, f_vars e -> Type.
+  Definition Free_Shape : Shape_System.
   Proof.
-    intros e x ; destruct x.
-    - exact (forall (E : c = c0),
-             (match E in (_ = y) return (f_vars y -> Type) with
-              | eq_refl => (fun z => P (f_inl c d z))
-              end) x).
-    - exact (forall (E : d = d0),
-             (match E in (_ = y) return (f_vars y -> Type) with
-              | eq_refl => (fun z => P (f_inr c d z))
-              end) x).
-    - exact unit.
-    - exact unit.
+    refine {|
+        Shape := f_cxt ;
+        positions := f_positions ;
+        shape_empty := f_empty ;
+        shape_coprod := f_coprod ;
+        shape_extend := f_extend
+      |}.
+    - constructor.
+      intros P x; elim x.
+    - intros c d.
+      simple refine {|
+          coprod_inj1 := @inl (f_positions c) (f_positions d) ;
+          coprod_inj2 := @inr (f_positions c) (f_positions d) ;
+        |}.
+      + intros P f g x.
+        destruct x as [a|b].
+        * apply f.
+        * apply g.
+      + reflexivity.
+      + reflexivity.
+    - intro c.
+      simple refine {|
+          plusone_top := None ;
+          plusone_next := @Some (f_positions c)
+        |}.
+      * { intros P e f [x|].
+          - now apply f.
+          - exact e. }
+      * reflexivity.
+      * reflexivity.
   Defined.
 
-  (* Lemma extend_eq *)
-  (*       {c d : f_cxt} *)
-  (*       (P : f_vars (f_coprod c d) -> Type) *)
-  (*       (x : f_vars (f_coprod c d)) : *)
-  (*   P x = extend P (f_coprod c d) x. *)
-  (* Proof. *)
-  (*   inversion x. *)
-  (*   unfold extend; simpl. *)
-
-
-  Lemma f_cxt_is_coprod (c d : f_cxt) : is_coprod (f_vars (f_coprod c d)) (f_vars c) (f_vars d).
-  Proof.
-    simple refine {| 
-             coprod_inj1 := f_inl c d ;
-             coprod_inj2 := f_inr c d
-           |}.
-    - intros P G H x.
-      dependent induction x.
-
-      assert (forall (y : f_vars (f_coprod c
-
-
-  Definition FreeProtoCxt : ProtoCxtSystem.
-  Proof.
-    simple refine
-           {|
-             ProtoCxt := f_cxt ;
-             vars := f_vars ;
-             protocxt_coprod := f_coprod ;
-             protocxt_extend := f_extend
-           |}.
-    - intros c d.
-      simple refine 
-
-  
-
-End FreeProtoCxt.
+End Free_shapes.
 
 Section DeBruijn.
 
-  Inductive DBSet : nat -> Type :=
-    | DB_Zero : forall {n}, DBSet (S n)
-    | DB_Succ : forall {n}, DBSet n -> DBSet (S n).
+  Inductive DB_Set : nat -> Type :=
+    | zero_db : forall {n}, DB_Set (S n)
+    | succ_db : forall {n}, DB_Set n -> DB_Set (S n).
 
-  Fixpoint DB_inl (n m : nat) (x : DBSet n) : DBSet (n + m).
+  Fixpoint DB_inl (n m : nat) (x : DB_Set n) : DB_Set (n + m).
   Proof.
     destruct x.
-    - exact DB_Zero.
-    - now apply DB_Succ, DB_inl.
+    - exact zero_db.
+    - now apply succ_db, DB_inl.
   Defined.
 
-  Fixpoint DB_inr (n m : nat) (x : DBSet m) : DBSet (n + m).
+  Fixpoint DB_inr (n m : nat) (x : DB_Set m) : DB_Set (n + m).
   Proof.
     destruct n.
     - exact x.
-    - apply DB_Succ, DB_inr; exact x.
+    - apply succ_db, DB_inr; exact x.
   Defined.
+
+  Definition db_extend
+             {n m : nat}
+             (P : DB_Set (n + m) -> Type) :
+    forall k, DB_Set k -> Type.
+  Proof.
+    intro k.
+    destruct (beq_nat_decide k (n + m)) as [E|_].
+    - rewrite (proj1 (eq_nat_is_eq k (n + m)) E).
+      exact P.
+    - intros _ ; exact unit.
+  Defined.
+
+  Definition db_extend_inject
+             (n m : nat)
+             (P : DB_Set (n + m) -> Type)
+             (i : DB_Set (n + m)) :
+    P i -> db_extend P (n + m) i.
+  Proof.
+    intro x.
+    unfold db_extend.
+
+
+
+             
+
 
   Fixpoint DB_coprod_rect
            (n m : nat)
-           (P : DBSet (n + m) -> Type)
-           (G : forall (y : DBSet n), P (DB_inl n m y))
-           (H : forall (z : DBSet m), P (DB_inr n m z))
-           (x : DBSet (n + m)) :
+           (P : DB_Set (n + m) -> Type)
+           (G : forall (y : DB_Set n), P (DB_inl n m y))
+           (H : forall (z : DB_Set m), P (DB_inr n m z))
+           (x : DB_Set (n + m)) :
     P x.
   Proof.
     induction n.
@@ -139,7 +151,7 @@ Section DeBruijn.
     destruct n as [|n].
     - apply H.
     - dependent induction x.
-      + apply (G DB_Zero).
+      + apply (| zero_db).
       + apply (IH 
       
 
@@ -148,7 +160,7 @@ Section DeBruijn.
 
 
 
-  Definition DB_inl (n m : nat) (x : DBSet n) : DBSet (n + m).
+  Definition DB_inl (n m : nat) (x : DB_Set n) : DB_Set (n + m).
   Proof.
     destruct x as [k H].
     exists k.
@@ -157,7 +169,7 @@ Section DeBruijn.
     - now apply le_plus_l.
   Defined.
 
-  Definition DB_inr (n m : nat) (y : DBSet m) : DBSet (n + m).
+  Definition DB_inr (n m : nat) (y : DB_Set m) : DB_Set (n + m).
   Proof.
     destruct y as [k H].
     exists (n + k)%nat.
@@ -167,10 +179,10 @@ Section DeBruijn.
   Lemma lt_dec (n m : nat) : {n < m} + {m <= n}.
 
   Fixpoint DB_coprod_rect (n m : nat)
-           (P : DBSet (n + m) -> Type)
-           (G : forall (g : DBSet n), P (DB_inl n m g))
-           (H : forall (h : DBSet m), P (DB_inr n m h))
-           (x : DBSet (n + m)) :
+           (P : DB_Set (n + m) -> Type)
+           (G : forall (g : DB_Set n), P (DB_inl n m g))
+           (H : forall (h : DB_Set m), P (DB_inr n m h))
+           (x : DB_Set (n + m)) :
     P x.
   Proof.
     destruct x as [k L].
@@ -178,7 +190,7 @@ Section DeBruijn.
     - apply G.
   Admitted.
 
-  Lemma plus_is_coprod (n m : nat) : is_coprod (DBSet (n + m)) (DBSet n) (DBSet m).
+  Lemma plus_is_coprod (n m : nat) : is_coprod (DB_Set (n + m)) (DB_Set n) (DB_Set m).
   Proof.
     simple refine {|
         coprod_inj1 := DB_inl n m ;
@@ -192,11 +204,11 @@ Section DeBruijn.
     - reflexivity.
   Defined.
 
-  Definition DeBruijn : ProtoCxtSystem.
+  Definition DeBruijn : Shape_System.
   Proof.
     refine {| ProtoCxt := nat ;
-              vars := DBSet ;
-              protocxt_coprod := plus
+              vars := DB_Set ;
+              shape_coprod := plus
            |}.
     (* protoctx_is_coprod *)
     - apply plus_is_coprod.
@@ -204,7 +216,7 @@ Section DeBruijn.
     (* protoctx_extend *)
     (* protoctx_is_extend *)
 
-  simple refine (Build_ProtoCxtSystem _ _ _ _ _ _).
+  simple refine (Build_Shape_System _ _ _ _ _ _).
   - exact nat.
   - exact Fin.t. (* should be fin *)
   - admit. (* should be + *)
