c561a418d2353bf775f4d02e5b7caec7b89f7a18
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2016-11-29 18:05:45 +0100

    Added De Bruijn Fixpoint shape system.

--- Examples.v
+++ Examples.v
@@ -3,6 +3,42 @@ Require Import Auxiliary.
 Require Import RawSyntax.
 
 Section Types_as_shapes.
+
+  Definition is_empty_Empty : is_empty Empty.
+  Proof.
+    constructor.
+    intros P x; elim x.
+  Defined.
+
+  Definition is_coprod_sum (A B : Type)
+    : is_coprod (sum A B) A B.
+  Proof.
+    simple refine {|
+        coprod_inj1 := @inl A B ;
+        coprod_inj2 := @inr A B ;
+      |}.
+    - intros P f g x.
+      destruct x as [a|b].
+      + apply f.
+      + apply g.
+    - reflexivity.
+    - reflexivity.
+  Defined.
+
+  Definition is_plusone_option (A : Type)
+    : is_plusone (option A) A.
+  Proof.
+    simple refine {|
+        plusone_top := None ;
+        plusone_next := @Some A
+        |}.
+    - intros P e f [x|].
+      + now apply f.
+      + exact e.
+    - reflexivity.
+    - reflexivity.
+  Defined.
+
   Definition Type_Shape : Shape_System.
   Proof.
     refine {|
@@ -12,29 +48,9 @@ Section Types_as_shapes.
         shape_coprod := sum ;
         shape_extend := option
       |}.
-    - constructor.
-      intros P x; elim x.
-    - intros A B.
-      simple refine {|
-          coprod_inj1 := @inl A B ;
-          coprod_inj2 := @inr A B ;
-        |}.
-      + intros P f g x.
-        destruct x as [a|b].
-        * apply f.
-        * apply g.
-      + reflexivity.
-      + reflexivity.
-    - intro A.
-      simple refine {|
-          plusone_top := None ;
-          plusone_next := @Some A
-        |}.
-      * { intros P e f [x|].
-          - now apply f.
-          - exact e. }
-      * reflexivity.
-      * reflexivity.
+    - apply is_empty_Empty.
+    - apply is_coprod_sum.
+    - apply is_plusone_option.
   Defined.
 
 End Types_as_shapes.
@@ -62,29 +78,9 @@ Section Free_shapes.
         shape_coprod := f_coprod ;
         shape_extend := f_extend
       |}.
-    - constructor.
-      intros P x; elim x.
-    - intros c d.
-      simple refine {|
-          coprod_inj1 := @inl (f_positions c) (f_positions d) ;
-          coprod_inj2 := @inr (f_positions c) (f_positions d) ;
-        |}.
-      + intros P f g x.
-        destruct x as [a|b].
-        * apply f.
-        * apply g.
-      + reflexivity.
-      + reflexivity.
-    - intro c.
-      simple refine {|
-          plusone_top := None ;
-          plusone_next := @Some (f_positions c)
-        |}.
-      * { intros P e f [x|].
-          - now apply f.
-          - exact e. }
-      * reflexivity.
-      * reflexivity.
+    - apply is_empty_Empty.
+    - intros. apply is_coprod_sum.
+    - intros. apply is_plusone_option.
   Defined.
 
 End Free_shapes.
@@ -94,20 +90,6 @@ Section DeBruijn.
   Inductive DB_positions : nat -> Type :=
     | zero_db : forall {n}, DB_positions (S n)
     | succ_db : forall {n}, DB_positions n -> DB_positions (S n).
-
-  Fixpoint DB_inl (n m : nat) (x : DB_positions n) : DB_positions (n + m).
-  Proof.
-    destruct x.
-    - exact zero_db.
-    - apply succ_db, DB_inl; exact x.
-  Defined.
-
-  Fixpoint DB_inr (n m : nat) (x : DB_positions m) : DB_positions (n + m).
-  Proof.
-    destruct n.
-    - exact x.
-    - apply succ_db, DB_inr; exact x. (* XXX Here "now" fails with f_equal. *)
-  Defined.
 
   Lemma DB_is_empty : is_empty (DB_positions 0).
   Proof.
@@ -126,12 +108,28 @@ Section DeBruijn.
              (i : DB_positions k)
     := forall (p : k = n.+1), P (transport DB_positions p i).
 
+  Fixpoint DB_inl (n m : nat) (x : DB_positions n) : DB_positions (n + m).
+  Proof.
+    destruct x.
+    - exact zero_db.
+    - apply succ_db, DB_inl; exact x.
+  Defined.
+
+  Fixpoint DB_inr (n m : nat) (x : DB_positions m) : DB_positions (n + m).
+  Proof.
+    destruct n.
+    - exact x.
+    - now apply succ_db, DB_inr; exact x.
+  Defined.
+
   Lemma S_injective {n m : nat} : S n = S m -> n = m.
   Proof.
     intro p. exact (ap pred p).
   Defined.
 
-  (* NOTE: this is the only sticking point for computation of [DB_is_plusone], [DB_is_coprod].  If we could find an alternative proof of [ap_S_S_injective] which makes [ap_S_S_injective_idpath] a judgemental equality, then those would all compute. *)
+  (* NOTE: this is the only sticking point for computation of [DB_is_plusone], [DB_is_coprod].  If we could find an alternative proof of [ap_S_S_injective] which makes [ap_S_S_injective_idpath] a judgemental equality, then those would all compute.
+
+  As it is, they will compute just when applied to actual numerals, but not for general numbers. *)
   Lemma ap_S_S_injective {n m : nat} (p : S n = S m) :
     ap S (S_injective p) = p.
   Proof.
@@ -143,7 +141,6 @@ Section DeBruijn.
   Proof.
     apply path_ishprop.
   Defined.
-  
 
   Lemma DB_is_plusone (n : nat) : is_plusone (DB_positions (n.+1)) (DB_positions n).
   Proof.
@@ -152,25 +149,25 @@ Section DeBruijn.
              plusone_next := succ_db |}.
     - intros P x f.
       transparent assert (H : (forall k (i : DB_positions k), DB_ext P i)).
-      + intros k i.
-        { destruct i as [l|l].
-          - intro p.
-            pose (q := S_injective p).
-            refine (transport 
-              (fun e => P (transport DB_positions e _))
-              (ap_S_S_injective p : ap S q = p) _).
-            clearbody q ; clear p.
-            destruct q.
-            exact x.            
-          - intro p.
-            pose (q := S_injective p).
-            refine (transport 
-              (fun e => P (transport DB_positions e _))
-              (ap_S_S_injective p : ap S q = p) _).
-            clearbody q ; clear p.
-            destruct q.
-            apply f. }
-      + intro y. apply (H (n .+1) y idpath).
+      { intros k i.
+        destruct i as [l|l].
+        * intro p.
+          pose (q := S_injective p).
+          refine (transport 
+            (fun e => P (transport DB_positions e _))
+            (ap_S_S_injective p : ap S q = p) _).
+          clearbody q ; clear p.
+          destruct q.
+          exact x.            
+        * intro p.
+          pose (q := S_injective p).
+          refine (transport 
+            (fun e => P (transport DB_positions e _))
+            (ap_S_S_injective p : ap S q = p) _).
+          clearbody q ; clear p.
+          destruct q.
+          apply f. }
+      intro y. apply (H (n .+1) y idpath).
     - intros P x f. simpl.
       rewrite ap_S_S_injective_idpath. apply idpath.
     - intros P x f k. simpl.
@@ -218,15 +215,88 @@ Section DeBruijn.
               shape_coprod := (fun n m => (n + m)%nat) ;
               shape_extend := S
            |}.
-    (* shape_is_empty *)
     - apply DB_is_empty.
-    (* shape_is_coprod *)
     - apply DB_is_coprod.
-    (* shape_is_plusone *)
     - apply DB_is_plusone.
   Defined.
 
 End DeBruijn.
+
+(* A second de Bruijn style shape system, defining the positions as a fixpoint instead of an inductive family. *)
+ 
+Section DeBruijn_Fixpoint.
+
+  Fixpoint DBF_positions (n : nat) : Type :=
+    match n with
+    | 0 => Empty
+    | n'.+1 => option (DBF_positions n')
+    end.
+
+  Definition zero_dbf {n} : DBF_positions (n.+1)
+  := None.
+
+  Definition succ_dbf {n} (i : DBF_positions n) : DBF_positions (n.+1)
+  := Some i.
+
+  Fixpoint DBF_inl (n m : nat) (i : DBF_positions n) {struct n}
+    : DBF_positions (n + m).
+  Proof.
+    destruct n as [ | n'].
+    - destruct i.
+    - destruct i as [ j | ].
+      + exact (succ_dbf (DBF_inl _ _ j)).
+      + exact zero_dbf.
+  Defined.
+
+  Fixpoint DBF_inr (n m : nat) (i : DBF_positions m) {struct n}
+    : DBF_positions (n + m).
+  Proof.
+    destruct n as [ | n'].
+    - exact i.
+    - apply succ_dbf, DBF_inr, i.
+  Defined.
+
+  Lemma DBF_is_coprod (n m : nat) :
+    is_coprod (DBF_positions (n + m)) (DBF_positions n) (DBF_positions m).
+  Proof.
+    simple refine
+      {| coprod_inj1 := DBF_inl _ _
+      ;  coprod_inj2 := DBF_inr _ _
+      |}.
+    (* coprod_rect *)
+    - induction n as [ | n' IH]; intros P f1 f2.
+      + exact f2.
+      + apply (plusone_rect _ _ (is_plusone_option _)); simpl.
+        * exact (f1 (zero_dbf)).
+        * apply IH.
+          -- intros i1. exact (f1 (succ_dbf i1)).
+          -- exact f2.
+    (* coprod_comp1 *)
+    - induction n as [ | n' IH]; intros P f1 f2.
+      + intros [].
+      + apply (plusone_rect _ _ (is_plusone_option _)).
+        * apply idpath.
+        * intros i1. exact (IH _ _ _ i1). 
+    (* coprod_comp2 *)
+    - induction n as [ | n' IH]; intros P f1 f2.
+      + intros i2; apply idpath.
+      + intros i2. exact (IH _ _ _ i2).
+  Defined.
+
+  Definition DeBruijn_Fixpoint : Shape_System.
+  Proof.
+    refine {| Shape := nat ;
+              positions := DBF_positions ;
+              shape_empty := 0 ;
+              shape_coprod := (fun n m => (n + m)%nat) ;
+              shape_extend := S
+           |}.
+    - apply is_empty_Empty.
+    - apply DBF_is_coprod.
+    - intros; apply is_plusone_option.
+  Defined.
+
+End DeBruijn_Fixpoint.
 
 (* TODO: variables as strings, or as natural numbers *)
 
