78d25abf85209fbc467d2a10f8f6419f87e5bf83
Andrej Bauer <Andrej.Bauer@andrej.com>
2018-03-24 23:51:34 +0100

    Conform `Auxiliary.WellFoundedRelation` to new naming conventions.

--- Auxiliary/WellFoundedRelation.v
+++ Auxiliary/WellFoundedRelation.v
@@ -1,8 +1,8 @@
 Require Import HoTT.
 
-Definition Well_founded {X : Type} (R : relation X)
+Definition well_founded {X : Type} (R : relation X)
   := forall P : X -> Type,
-     (forall x:X, (forall y:X, R y x -> P y) -> P x)
-     -> (forall x:X, P x).
+     (forall x : X, (forall y : X, R y x -> P y) -> P x)
+     -> (forall x : X, P x).
 
 
