de0618628b4b51a133e7a5c1b1ddbe570d64fc71
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2018-03-30 16:04:04 +0200

    Gave statement of unique typing metatheorem.

--- Metatheorems/UniqueTyping.v
+++ Metatheorems/UniqueTyping.v
@@ -1,3 +1,22 @@
 
 
-(* TODO: theorem stating unique typing, for any (fully typed) type theory. *)
+Require Import Auxiliary.Family.
+Require Import Proto.ShapeSystem.
+Require Import Raw.Syntax.
+Require Import Raw.TypeTheory.
+Require Import Typed.Derivation.
+Require Import Typed.TypeTheory.
+
+(* Main goal of this file: theorem stating unique typing, for any (fully typed) type theory. *)
+
+Theorem unique_typing {σ : shape_system} (T : Type_Theory σ)
+    (Σ := Signature_of_Type_Theory T)
+    (Γ : raw_context Σ) (a : raw_term Σ Γ) (A A' : raw_type Σ Γ)
+    (d : Derivation_from_Type_Theory T [<>] [Tm! Γ |- a ; A !])
+    (d' : Derivation_from_Type_Theory T [<>] [Tm! Γ |- a ; A' !])
+  : Derivation_from_Type_Theory T [<>] [TyEq! Γ |- A ≡ A' !].
+Abort.
+
+(* Sketch proof: ????
+
+Presumably: will need to generalised to derivations with hypotheses — specifcally, to derivations over an arbitrary _algebraic extension_ of a type theory. *)
