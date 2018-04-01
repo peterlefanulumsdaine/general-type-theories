035873b26d134ede43f2669e7e0ac4ec911394fc
Andrej Bauer <Andrej.Bauer@andrej.com>
2018-04-01 23:34:35 +0200

    `Typed/FlatRule.v` is never used, it looks like we can delete it.

--- Typed/FlatRule.v
+++ Typed/FlatRule.v
@@ -11,20 +11,21 @@ Section WellTypedFlatRule.
 
   Context {σ : shape_system}.
 
-  Local Definition is_well_typed {Σ : signature σ}
-    (T : flat_type_theory Σ) (r : flat_rule Σ) : Type.
-  Proof.
-    refine (_ * _).
-    - exact (forall p : flat_rule_premises _ r,
-        FlatTypeTheory.presupposition_derivation
-          (FlatTypeTheory.fmap include_symbol T)
-          (boundary_of_judgement (projT2 (flat_rule_premises _ _ p)))
-          (flat_rule_premises _ r)).
-    - exact (FlatTypeTheory.presupposition_derivation
-          (FlatTypeTheory.fmap include_symbol T)
-          (boundary_of_judgement (projT2 (flat_rule_conclusion _ r)))
-          (flat_rule_premises _ r)).
-  Defined.
+  (* NOTE: THIS IS NEVER USED. SHOULD WE DELETE IT? *)
+  (* Local Definition is_well_typed {Σ : signature σ} *)
+  (*   (T : flat_type_theory Σ) (r : flat_rule Σ) : Type. *)
+  (* Proof. *)
+  (*   refine (_ * _). *)
+  (*   - exact (forall p : flat_rule_premises _ r, *)
+  (*       FlatTypeTheory.presupposition_derivation *)
+  (*         (FlatTypeTheory.fmap include_symbol T) *)
+  (*         (boundary_of_judgement (projT2 (flat_rule_premises _ _ p))) *)
+  (*         (flat_rule_premises _ r)). *)
+  (*   - exact (FlatTypeTheory.presupposition_derivation *)
+  (*         (FlatTypeTheory.fmap include_symbol T) *)
+  (*         (boundary_of_judgement (projT2 (flat_rule_conclusion _ r))) *)
+  (*         (flat_rule_premises _ r)). *)
+  (* Defined. *)
 (* TODO: make signature implicit in args of [flat_rule] fields. 
    TODO: rename [flat_rule_premises] to [flat_rule_premise] *)
 
