42c53639d238742013bc492876091462dba95a5b
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2018-04-03 22:01:17 +0200

    Improved documentation of structural rules

--- Raw/RawStructuralRule.v
+++ Raw/RawStructuralRule.v
@@ -41,6 +41,12 @@ Context (Σ : @signature σ).
 
 Section ContextRules.
 
+(* The empty context rule:
+
+  ---------------
+  |-  .   cxt
+
+*)
 Local Definition ctx_empty : Closure.rule (judgement_total Σ).
 Proof.
   split.
@@ -50,6 +56,14 @@ Proof.
   - exact [Cxt! |- [::] !].
 Defined.
 
+(* The context extension rule:
+
+   |- Γ cxt
+   Γ |- A type
+   ----------------
+   |- Γ, x:A cxt
+
+*)
 Local Definition ctx_extend : Closure.system (judgement_total Σ).
 Proof.
   exists { Γ : raw_context Σ & raw_type Σ Γ }.
@@ -96,8 +110,13 @@ End ContextRules.
 
 Section SubstitutionRules.
 
-(** General substitution along context maps. *)
+(** General substitution along context maps:
 
+  Γ' |- f(x) : A   [for each x in Γ, A := type of x in Γ]
+  Γ |- J   [for J any hypothetical judgement]
+  --------------------
+  Γ' |- f^*J
+*)
 Local Definition subst_apply : Closure.system (judgement_total Σ).
 Proof.
   exists { Γ : raw_context Σ
@@ -124,7 +143,13 @@ Proof.
     intros i. exact (substitute f (hjfi i)).
 Defined.
 
-(** Substitution respects *equality* of context morphisms *)
+(** Substitution respects *equality* of context morphisms:
+
+  Γ' |- f(x) = g(x) : A   [for each x in Γ, A := type of x in Γ]
+  Γ |- J   [for J any hypothetical judgement]
+  --------------------
+  Γ' |- f^*J = g^*J  [ for J any object judgement ]
+ *)
 Local Definition subst_equal : Closure.system (judgement_total Σ).
 Proof.
   exists {   Γ : raw_context Σ
@@ -183,20 +208,17 @@ Section HypotheticalStructuralRules.
 
 *)
 
-(* The variable rule:
+(* The general variable rule:
 
-  |– A type
-  -----------
-  x:A |– x:A
+  Γ |- A type
+  ------------- (x in Γ, A := type of x in Γ)
+  Γ |- x : A 
 
 *)
 
   Local Definition variable : Closure.system (judgement_total Σ).
   (* TODO: this form is not sufficient — we want the version for an arbitrary variable in the context, i.e.
   
-  Γ |- A type
-  ------------- (x in Γ, A := type of x in Γ)
-  Γ |- x : A 
    *)
 Proof.
   apply FlatRule.closure_system.
