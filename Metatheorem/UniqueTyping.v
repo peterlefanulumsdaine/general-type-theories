ff5f3991f00771ba9925d8ac148111479f5413ae
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2020-08-04 17:48:32 +0100

    Updated statement of tightness metatheorem

--- Metatheorem/UniqueTyping.v
+++ Metatheorem/UniqueTyping.v
@@ -2,21 +2,20 @@
 Require Import Auxiliary.Family.
 Require Import Syntax.ScopeSystem.
 Require Import Syntax.All.
-Require Import Typing.Context.
-Require Import Typing.Judgement.
-Require Import Presented.RawTypeTheory.
-Require Import Presented.TypeTheory.
+Require Import Typing.All.
 
-(* Main goal of this file: theorem stating unique typing, for any (fully typed) type theory. *)
+Definition tight_type_theory {σ} {Σ : signature σ} (T : flat_type_theory Σ)
+  : Type.
+Admitted. (* TODO: define tightness *)
 
-Theorem unique_typing {σ : scope_system} (T : raw_type_theory σ)
-    (Σ := RawTypeTheory.signature T)
+(* Main goal of this file: theorem stating unique typing, for any tight type theory. *)
+
+Theorem unique_typing {σ} {Σ : signature σ} (T : flat_type_theory Σ)
+    (T_tight : tight_type_theory T)
     (Γ : raw_context Σ) (a : raw_term Σ Γ) (A A' : raw_type Σ Γ)
-    (d : RawTypeTheory.derivation T [<>] [! Γ |- a ; A !])
-    (d' : RawTypeTheory.derivation T [<>] [! Γ |- a ; A' !])
-  : RawTypeTheory.derivation T [<>] [! Γ |- A ≡ A' !].
+    (d : FlatTypeTheory.derivation T [<>] [! Γ |- a ; A !])
+    (d' : FlatTypeTheory.derivation T [<>] [! Γ |- a ; A' !])
+  : FlatTypeTheory.derivation T [<>] [! Γ |- A ≡ A' !].
 Abort.
 
-(* Sketch proof: ????
-
-Presumably: will need to generalised to derivations with hypotheses — specifcally, to derivations over an arbitrary _algebraic  extension_ of a type theory. *)
+(* Sketch proof: see paper. *)
